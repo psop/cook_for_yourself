153d9d7611c53c55fa593deef445a442784a92cb
LizHuang <search.psop@gmail.com.tw>
2017-08-21 16:57:08 +0800

    Add products#show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,9 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all.order("created_at DESC")
+  end
+
+  def show
+  	@product = Product.find(params[:id])
+  end
 end
