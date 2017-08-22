7a0ac35df8712f531be398d077a9da26db8a2934
LizHuang <search.psop@gmail.com.tw>
2017-08-22 16:54:27 +0800

    admin/orders#index & #show

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,14 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
     resources :users do
       member do
         post :to_admin
