53a0eab3e56187c699b0e7f76154631832f82633
LizHuang <search.psop@gmail.com.tw>
2017-08-22 11:28:45 +0800

    Add product to the cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,17 @@ class ProductsController < ApplicationController
   def show
   	@product = Product.find(params[:id])
   end
+
+  def add_to_cart
+  	@product = Product.find(params[:id])
+
+    if !current_cart.items.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "#{@product.title} has been put in the cart!"
+    else
+      flash[:warning] = "You already have this item."
+    end
+
+    redirect_to :back
+  end
 end
