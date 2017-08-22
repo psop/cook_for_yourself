004eaa591eb9455c6e7f42127e5490ff0d609616
LizHuang <search.psop@gmail.com.tw>
2017-08-22 18:07:36 +0800

    fix mailer

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,7 +8,7 @@ class OrdersController < ApplicationController
       @order.build_item_cache_from_cart(current_cart)
       @order.calculate_total!(current_cart)
       current_cart.clean!
-      OrderMailer.delay.notify_order_placed(@order)
+      OrderMailer.notify_order_placed(@order)
 
       redirect_to order_path(@order.token)
     else
