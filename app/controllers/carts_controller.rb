d0927b611c9ea721bce4aa2bbba93ffa3e3c9b56
LizHuang <search.psop@gmail.com.tw>
2017-08-22 14:36:12 +0800

    Reset cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,4 +5,10 @@ class CartsController < ApplicationController
   	@order = current_user.orders.build
   	@info = @order.build_info
   end
+
+  def clean
+    current_cart.clean!
+    flash[:warning] = "Cart has been reset."
+    redirect_to carts_path
+  end
 end
