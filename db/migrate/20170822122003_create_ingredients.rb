a3ac9d98385d2648017f0104a9c3e497e1310bdd
LizHuang <search.psop@gmail.com.tw>
2017-08-25 16:16:49 +0800

    delete wrong migration

--- db/migrate/20170822122003_create_ingredients.rb
+++ db/migrate/20170822122003_create_ingredients.rb
@@ -2,7 +2,6 @@ class CreateIngredients < ActiveRecord::Migration
   def change
     create_table :ingredients do |t|
       t.string :name
-      t.belongs_to :recipe, index: true, foreign_key: true
 
       t.timestamps null: false
     end
--- db/migrate/20170822123343_create_directions.rb
+++ db/migrate/20170822123343_create_directions.rb
@@ -2,7 +2,6 @@ class CreateDirections < ActiveRecord::Migration
   def change
     create_table :directions do |t|
       t.text :step
-      t.belongs_to :recipe, index: true, foreign_key: true
 
       t.timestamps null: false
     end
